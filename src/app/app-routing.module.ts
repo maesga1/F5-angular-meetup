60ae66594d4d2ceb3a9c223aee4e41b7e1f12a00
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-08-30 09:11:41 +0200

    Header component css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './animals/views/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  
+  { path: '', redirectTo: '/header', pathMatch: 'full' },
+  { path: 'header', component: HeaderComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'footer', component: FooterComponent },
+ 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
